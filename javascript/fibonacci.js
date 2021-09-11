19d05cd111cc7367380e0a6198a15c5858150088
Nicholas Thomson <nicholasgraythomson@gmail.com>
2021-09-11 11:27:44 -0400

    up to 300

--- javascript/fibonacci.js
+++ javascript/fibonacci.js
@@ -2,21 +2,22 @@ function fibonacci(num) {
   if (num == 0){
     return 0
   }
+
   else {
-let a = 0
-let b = 1
-let result = b
+    let a = 0
+    let b = 1
+    let result = b
 
-for (let i = 1; i < num; i++){
-  
-  result = a + b
-  a = b
-  b = result
-}
-return(result)
-}
-}
+    for (let i = 1; i < num; i++){
+      result = a + b
+      a = b
+      b = result
+    }
+    return result
 
+  }
+
+}
 if (require.main === module) {
   // add your own tests in here
   console.log("Expecting: 0");
@@ -31,22 +32,12 @@ if (require.main === module) {
 
   console.log("Expecting: 55");
   console.log("=>", fibonacci(10));
+
+  console.log("Expecting: 222232244629420445529739893461909967206666939096499764990979600")
+  console.log("=>", fibonacci(300))
 }
 
 module.exports = fibonacci;
 
 // Please add your pseudocode to this file
 // And a written explanation of your solution
-
-
-// let a = 0
-// let b = 1
-// let result = b
-
-// for (let i = 1; i < 100; i ++){
-//   console.log(result)
-//   result = a + b
-//   a = b
-//   b = result
-  
-// }
